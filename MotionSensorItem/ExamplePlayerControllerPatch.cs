d17cf0dd1031aa00080f92262f6dcf8546b8aeb3
the-ryan-12 <195170798+the-ryan-12@users.noreply.github.com>
2025-04-03 14:30:03 -0400

    Initial Commit

---
+++ MotionSensorItem/ExamplePlayerControllerPatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace MotionSensorItem;
+
+[HarmonyPatch(typeof(PlayerController))]
+static class ExamplePlayerControllerPatch
+{
+    [HarmonyPrefix, HarmonyPatch(nameof(PlayerController.Start))]
+    private static void Start_Prefix(PlayerController __instance)
+    {
+        // Code to execute for each PlayerController *before* Start() is called.
+        MotionSensorItem.Logger.LogDebug($"{__instance} Start Prefix");
+    }
+
+    [HarmonyPostfix, HarmonyPatch(nameof(PlayerController.Start))]
+    private static void Start_Postfix(PlayerController __instance)
+    {
+        // Code to execute for each PlayerController *after* Start() is called.
+        MotionSensorItem.Logger.LogDebug($"{__instance} Start Postfix");
+    }
+}
---
+++ MotionSensorItem/MotionSensorItem.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace MotionSensorItem;
+
+[BepInPlugin("Vee.MotionSensorItem", "MotionSensorItem", "1.0")]
+public class MotionSensorItem : BaseUnityPlugin
+{
+    internal static MotionSensorItem Instance { get; private set; } = null!;
+    internal new static ManualLogSource Logger => Instance._logger;
+    private ManualLogSource _logger => base.Logger;
+    internal Harmony? Harmony { get; set; }
+
+    private void Awake()
+    {
+        Instance = this;
+        
+        // Prevent the plugin from being deleted
+        this.gameObject.transform.parent = null;
+        this.gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+        Patch();
+
+        Logger.LogInfo($"{Info.Metadata.GUID} v{Info.Metadata.Version} has loaded!");
+    }
+
+    internal void Patch()
+    {
+        Harmony ??= new Harmony(Info.Metadata.GUID);
+        Harmony.PatchAll();
+    }
+
+    internal void Unpatch()
+    {
+        Harmony?.UnpatchSelf();
+    }
+
+    private void Update()
+    {
+        // Code that runs every frame goes here
+    }
+}
